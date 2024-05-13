21e9e4631885dfd74ac8ba86b290149dc3a03a93
Christopher Kelly <cck197@cck197.net>
2024-05-13 06:27:03 -0700

    useEventSource from Remix Utils made it impossible to manage state correctly

--- app/routes/app.stream.jsx
+++ app/routes/app.stream.jsx
@@ -6,50 +6,41 @@ import {
   BlockStack,
   TextField,
 } from "@shopify/polaris";
-import { useState, useEffect, useMemo } from "react";
-
-import { useEventSource, EventSourceProvider } from "remix-utils/sse/react";
+import { useState, useEffect, useRef } from "react";
 import { json } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 import { useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
-
   return json({ url: process.env.SSE_URL });
 };
 
 export default function Index() {
   const { url } = useLoaderData();
-
-  const map = useMemo(() => new Map(), []);
-
   const [message, setMessage] = useState("");
-
-  const event = useEventSource(url);
+  const eventSourceRef = useRef(null);
 
   useEffect(() => {
-    function handleData() {
-      if (event) {
-        console.log("event", event);
-        const data = JSON.parse(event);
-        if (data.event === "end") {
-          console.log("disconnecting...");
-          map.forEach((value, key) => {
-            value.source.close(); // close each EventSource
-          });
-          map.clear();
-          setTimeout(() => {
-            setMessage("");
-          }, 2000);
-        } else {
-          setMessage((prevMessage) => prevMessage + data.message);
-        }
-      }
-    }
+    if (!url) return;
 
-    handleData();
-  }, [event, map, setMessage]);
+    const eventSource = new EventSource(url);
+    eventSourceRef.current = eventSource;
+
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.event === "end") {
+        console.log("disconnecting...");
+        eventSource.close();
+      } else {
+        setMessage((prevMessage) => prevMessage + data.message);
+      }
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, [url]);
 
   return (
     <Page>
@@ -62,13 +53,12 @@ export default function Index() {
                 <Text as={"h2"} variant="headingLg">
                   Streaming Output
                 </Text>
-                <EventSourceProvider value={map}>
-                  <TextField
-                    value={message}
-                    multiline="true"
-                    autoComplete="off"
-                  />
-                </EventSourceProvider>
+                <TextField
+                  value={message}
+                  multiline="true"
+                  autoComplete="off"
+                  readOnly={true}
+                />
               </BlockStack>
             </Card>
           </BlockStack>
